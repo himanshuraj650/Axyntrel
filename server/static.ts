622419b8d598812456f25c084c171c88168a4cb9
Himanshu Raj <himanshuraj650@gmail.com>
2026-03-05 22:11:45 +0530

    fix static path for render

--- server/static.ts
+++ server/static.ts
@@ -3,12 +3,11 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Fix for __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
+  const distPath = path.resolve(__dirname, "../dist");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
@@ -18,7 +17,6 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
   app.use("/*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
