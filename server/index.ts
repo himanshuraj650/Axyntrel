5d8b45736cd74858558246fe392ec8524ed1222c
Himanshu Raj <himanshuraj650@gmail.com>
2026-03-05 21:39:11 +0530

    first commit

--- server/index.ts
+++ server/index.ts
@@ -92,7 +92,7 @@ app.use((req, res, next) => {
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
 
-httpServer.listen(port, () => {
+httpServer.listen(port, "0.0.0.0", () => {
   log(`serving on port ${port}`);
 });
 })();
